3c4a5dc1fda894997f8734e1008aa8303ef143b1
cjo3o <cjy55973743@gmail.com>
2024-04-17 14:48:56 +0900

    재업로드

---
+++ Assets/Script/OptionManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionManager : MonoBehaviour
+{
+    public void setVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+}
