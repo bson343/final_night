8f4a89351dd5da7c9aed209b1d91768a5fd0bbf1
KangUnMan <105927570+KangUnMan@users.noreply.github.com>
2024-04-22 04:36:10 +0900

    feat : 노드에 따라 다른 이벤트

--- Assets/Scripts/MapNode.cs
+++ Assets/Scripts/MapNode.cs
@@ -16,49 +16,51 @@ namespace Map
 
 namespace Map
 {
-    public class MapNode : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+    public class MapNode : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler 
+        //Unity에서 사용되는 게임 오브젝트로, UI 요소와 상호 작용할 수 있으며 이벤트에 대응하여 특정 동작을 수행가능
     {
-        public SpriteRenderer sr;
-        public Image image;
-        public SpriteRenderer visitedCircle;
-        public Image circleImage;
-        public Image visitedCircleImage;
+        public SpriteRenderer sr; // Unity에서 2D 스프라이트를 렌더링하는 데 사용되는 컴포넌트
+        public Image image; //Unity UI에서 이미지를 나타내는 컴포넌트
+        public SpriteRenderer visitedCircle; //방문한 노드를 표시하는 데 사용되는 스프라이트 렌더러
+        public Image circleImage; //노드 주변에 원 형태의 이미지를 표시하는 데 사용되는 이미지 (방문한 노드에 동그라미 쳐짐)
+        public Image visitedCircleImage; //방문한 노드 주변에 표시되는 특정 이미지
 
-        public Node Node { get; private set; }
-        public NodeBlueprint Blueprint { get; private set; }
+        public Node Node { get; private set; } //Node 클래스의 인스턴스를 나타내는 속성으로, 외부에서 읽을 수 있지만 수정은 불가능
+        public NodeBlueprint Blueprint { get; private set; } //NodeBlueprint 클래스의 인스턴스
 
-        private float initialScale;
-        private const float HoverScaleFactor = 1.2f;
-        private float mouseDownTime;
+        private float initialScale; // 초기 크기 저장 변수
+        private const float HoverScaleFactor = 1.2f; //마우스를 올렸을 때 크기를 조절하기 위한 상수
+        private float mouseDownTime; //마우스 버튼이 눌린 시간을 저장하는 변수
 
-        private const float MaxClickDuration = 0.5f;
+        private const float MaxClickDuration = 0.5f; //클릭한 시간이 유효한 최대 시간을 나타내는 상수
 
-        public void SetUp(Node node, NodeBlueprint blueprint)
+        public void SetUp(Node node, NodeBlueprint blueprint) //MapNode 클래스의 인스턴스를 설정하는 데 사용
+                                                              //주어진 노드와 노드 블루프린트를 기반으로 해당 MapNode의 상태를 초기화
         {
             Node = node;
             Blueprint = blueprint;
-            if (sr != null) sr.sprite = blueprint.sprite;
-            if (image != null) image.sprite = blueprint.sprite;
-            if (node.nodeType == NodeType.Boss) transform.localScale *= 1.5f;
-            if (sr != null) initialScale = sr.transform.localScale.x;
-            if (image != null) initialScale = image.transform.localScale.x;
+            if (sr != null) sr.sprite = blueprint.sprite; //sr (SpriteRenderer)가 null이 아니라면, 노드의 블루프린트에 있는 스프라이트를 sr.sprite에 할당
+            if (image != null) image.sprite = blueprint.sprite; //image가 null이 아니라면, 노드의 블루프린트에 있는 스프라이트를 image.sprite에 할당
+            if (node.nodeType == NodeType.Boss) transform.localScale *= 1.5f; //만약 노드의 타입이 보스 노드라면, 해당 노드의 스케일을 1.5배로 증가
+            if (sr != null) initialScale = sr.transform.localScale.x; //sr이 null이 아니라면, 초기 스케일을 sr.transform.localScale.x에 저장
+            if (image != null) initialScale = image.transform.localScale.x; //image가 null이 아니라면, 초기 스케일을 image.transform.localScale.x에 저장
 
-            if (visitedCircle != null)
+            if (visitedCircle != null) //visitedCircle가 null이 아니라면, 해당 원의 색상을 MapView.Instance.visitedColor로 설정하고 비활성화(방문한 곳의 색상으로 바뀌고 클릭 못하게)
             {
                 visitedCircle.color = MapView.Instance.visitedColor;
                 visitedCircle.gameObject.SetActive(false);
             }
 
-            if (circleImage != null)
+            if (circleImage != null) //circleImage가 null이 아니라면, 해당 이미지의 색상을 MapView.Instance.visitedColor로 설정하고 비활성화
             {
                 circleImage.color = MapView.Instance.visitedColor;
                 circleImage.gameObject.SetActive(false);    
             }
             
-            SetState(NodeStates.Locked);
+            SetState(NodeStates.Locked); //노드를 잠긴 상태로 설정하기 위해 SetState(NodeStates.Locked)를 호출
         }
 
-        public void SetState(NodeStates state)
+        public void SetState(NodeStates state) // SetState 메서드는 MapNode의 상태를 변경하는 데 사용 (방문한곳, 닫힌곳 , 갈 수 있는 곳) 
         {
             if (visitedCircle != null) visitedCircle.gameObject.SetActive(false);
             if (circleImage != null) circleImage.gameObject.SetActive(false);
@@ -113,55 +115,56 @@ namespace Map
                     
                     break;
                 default:
-                    throw new ArgumentOutOfRangeException(nameof(state), state, null);
+                    throw new ArgumentOutOfRangeException(nameof(state), state, null); //상태가 지정되지 않은 경우(default)에는 ArgumentOutOfRangeException를 발생
             }
         }
 
-        public void OnPointerEnter(PointerEventData data)
+        public void OnPointerEnter(PointerEventData data) //마우스 포인터가 MapNode 오브젝트 위에 올라갔을 때 호출되는 Unity 이벤트 핸들러
         {
             if (sr != null)
             {
                 sr.transform.DOKill();
-                sr.transform.DOScale(initialScale * HoverScaleFactor, 0.3f);
+                sr.transform.DOScale(initialScale * HoverScaleFactor, 0.3f); //마우스를 올렸을 때 해당 오브젝트의 크기를 키우는 효과
             }
 
             if (image != null)
             {
                 image.transform.DOKill();
-                image.transform.DOScale(initialScale * HoverScaleFactor, 0.3f);
+                image.transform.DOScale(initialScale * HoverScaleFactor, 0.3f); //마우스를 올렸을 때 해당 오브젝트의 크기를 키우는 효과
             }
         }
 
-        public void OnPointerExit(PointerEventData data)
+        public void OnPointerExit(PointerEventData data) //마우스 포인터가 MapNode 오브젝트를 벗어날 때 호출되는 Unity 이벤트 핸들러
         {
             if (sr != null)
             {
                 sr.transform.DOKill();
-                sr.transform.DOScale(initialScale, 0.3f);
+                sr.transform.DOScale(initialScale, 0.3f); // 마우스가 벗어났을때 원래대로
             }
 
             if (image != null)
             {
                 image.transform.DOKill();
-                image.transform.DOScale(initialScale, 0.3f);
+                image.transform.DOScale(initialScale, 0.3f); //원래대로
             }
         }
 
-        public void OnPointerDown(PointerEventData data)
+        public void OnPointerDown(PointerEventData data) //마우스 버튼을 누르는 순간 호출되는 이벤트 핸들러
         {
-            mouseDownTime = Time.time;
+            mouseDownTime = Time.time; // 마우스 버튼을 누를때 누른 시간을 저장 (얼마나 오래 눌렀는가 계산할때 쓰인다.)
         }
 
-        public void OnPointerUp(PointerEventData data)
+        public void OnPointerUp(PointerEventData data) //마우스 버튼을 누른 후 해당 버튼을 뗄 때 호출되는 이벤트 핸들러
         {
-            if (Time.time - mouseDownTime < MaxClickDuration)
+            if (Time.time - mouseDownTime < MaxClickDuration) //약 시간 간격이 MaxClickDuration보다 작다면,
+                                                              
             {
-                // user clicked on this node:
+                // 유저가 노드를 클릭했다는걸 알린다.
                 MapPlayerTracker.Instance.SelectNode(this);
             }
         }
 
-        public void ShowSwirlAnimation()
+        public void ShowSwirlAnimation() //특정한 애니메이션을 사용하여 방문한 노드를 강조하는 데 사용
         {
             if (visitedCircleImage == null)
                 return;
--- Assets/Scripts/MapPlayerTracker.cs
+++ Assets/Scripts/MapPlayerTracker.cs
@@ -5,45 +5,45 @@ using UnityEngine;
 
 namespace Map
 {
-    public class MapPlayerTracker : MonoBehaviour
+    public class MapPlayerTracker : MonoBehaviour //게임에서 플레이어의 위치를 추적하고, 게임 맵에서 노드를 선택할 때 발생하는 동작을 처리
     {
-        public bool lockAfterSelecting = false;
-        public float enterNodeDelay = 1f;
-        public MapManager mapManager;
-        public MapView view;
+        public bool lockAfterSelecting = false; //플레이어가 노드를 선택한 후에 잠금 여부를 결정하는 불 변수
+        public float enterNodeDelay = 1f; //노드에 도착하기 전의 지연 시간을 나타냄
+        public MapManager mapManager; // 맵을 관리하는 MapManager 인스턴스
+        public MapView view; // 맵을 표시하는 MapView 인스턴스
 
         public static MapPlayerTracker Instance;
 
-        public bool Locked { get; set; }
+        public bool Locked { get; set; } // 현재 플레이어가 잠겨 있는지 여부를 나타내는 속성
 
         private void Awake()
         {
-            Instance = this;
+            Instance = this; //이렇게 게임 시작하기전 선언
         }
 
-        public void SelectNode(MapNode mapNode)
+        public void SelectNode(MapNode mapNode) //주어진 MapNode를 선택하는 데 필요한 동작을 처리
         {
-            if (Locked) return;
+            if (Locked) return; //Locked 속성이 true인 경우, 즉 플레이어가 현재 잠겨 있는 상태라면, 노드 선택을 처리하지 않고 함수를 종료
 
             // Debug.Log("Selected node: " + mapNode.Node.point);
 
-            if (mapManager.CurrentMap.path.Count == 0)
+            if (mapManager.CurrentMap.path.Count == 0) //현재 플레이어가 선택한 노드가 없는 경우.
+                                                       //이 경우 플레이어는 아직 노드를 선택하지 않았으므로, y 좌표가 0인 노드 중 하나를 선택할 수 있다.
             {
-                // player has not selected the node yet, he can select any of the nodes with y = 0
                 if (mapNode.Node.point.y == 0)
                     SendPlayerToNode(mapNode);
                 else
-                    PlayWarningThatNodeCannotBeAccessed();
+                    PlayWarningThatNodeCannotBeAccessed(); // 그렇지않으면 못접근한다고 알림
             }
-            else
+            else //현재 플레이어가 이미 선택한 노드가 있는 경우
             {
-                var currentPoint = mapManager.CurrentMap.path[mapManager.CurrentMap.path.Count - 1];
+                var currentPoint = mapManager.CurrentMap.path[mapManager.CurrentMap.path.Count - 1]; //플레이어가 선택한 마지막 노드(currentPoint)를 가져와서
                 var currentNode = mapManager.CurrentMap.GetNode(currentPoint);
 
-                if (currentNode != null && currentNode.outgoing.Any(point => point.Equals(mapNode.Node.point)))
+                if (currentNode != null && currentNode.outgoing.Any(point => point.Equals(mapNode.Node.point))) //연결되어 있다면 SendPlayerToNode 메소드를 호출하여 플레이어를 해당 노드로 이동
                     SendPlayerToNode(mapNode);
                 else
-                    PlayWarningThatNodeCannotBeAccessed();
+                    PlayWarningThatNodeCannotBeAccessed(); // 아니면 PlayWarningThatNodeCannotBeAccessed();메소드를 호출하여 접근 못한다고 알림
             }
         }
 
@@ -59,13 +59,13 @@ namespace Map
             DOTween.Sequence().AppendInterval(enterNodeDelay).OnComplete(() => EnterNode(mapNode));
         }
 
-        private static void EnterNode(MapNode mapNode)
+        private static void EnterNode(MapNode mapNode) // 플레이어를 선택한 노드로 이동시키는 역할 (여기서 코드를 수정하면 개발자가 원하는대로 노드에 따라 이벤트 수정가능)
         {
-            // we have access to blueprint name here as well
+            // 당 메서드에서 노드의 블루프린트 이름에 접근할 수 있다
             Debug.Log("Entering node: " + mapNode.Node.blueprintName + " of type: " + mapNode.Node.nodeType);
-            // load appropriate scene with context based on nodeType:
-            // or show appropriate GUI over the map: 
-            // if you choose to show GUI in some of these cases, do not forget to set "Locked" in MapPlayerTracker back to false
+            // 이것은 특정 유형의 노드에 따라 적절한 상황을 고려하여 적절한 장면을 로드
+            // 특정 유형의 노드에 따라 맵 위에 적절한 GUI(그래픽 사용자 인터페이스)를 표시
+            // GUI가 표시되는 특정 상황에서 Locked 속성을 false로 설정하여 플레이어에게 이동 및 상호작용의 자유를 제공해야 한다는 것을 개발자에게 상기
             switch (mapNode.Node.nodeType)
             {
                 case NodeType.MinorEnemy:
@@ -87,9 +87,9 @@ namespace Map
             }
         }
 
-        private void PlayWarningThatNodeCannotBeAccessed()
+        private void PlayWarningThatNodeCannotBeAccessed() // 디버그 출력
         {
-            Debug.Log("Selected node cannot be accessed");
+            Debug.Log("Selected node cannot be accessed"); // 선택한 노드에 접근할수없다.
         }
     }
 }
