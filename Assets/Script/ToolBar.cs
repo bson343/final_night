bcd2a5d87a99c5ce90ab29a4b117d85b40b2ece6
KangUnMan <dnsdud426@g.yju.ac.kr>
2024-04-24 13:58:06 +0900

    feat: 상단 툴바 초안

---
+++ Assets/Script/ToolBar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBar : MonoBehaviour
+{
+    void Awake() // ÆÄ±« ¾ÈµÊ
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
