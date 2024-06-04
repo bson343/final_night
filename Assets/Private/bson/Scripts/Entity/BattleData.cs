f49115bc279b6b5c14ef63c9abc48c1eaa4cd384
bson343 <byeonggukson@gmail.com>
2024-06-05 05:22:32 +0900

    BattleManager 테스트용 Mock Data

---
+++ Assets/Private/bson/Scripts/Entity/BattleData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoomData", menuName = "RoomData/BattleData")]
+public class BattleData : ScriptableObject
+{
+    [SerializeField]
+    private List<Enemy> enemies;
+    [SerializeField]
+    private List<Vector3> spawnPos;
+
+    public int maxMoney;
+    public int minMoney;
+
+    public List<Enemy> Enemies => enemies;
+    public List<Vector3> SpawnPos => spawnPos;
+}
