053e8087ae480a6e1dcbe1d222ad7885bdb4f14f
SGH1423423 <sagong0506@Gmail.com>
2024-09-23 15:46:21 +0900

    상태 아이콘 추가 및 공격 애니메이션 제작 중

---
+++ Assets/Status/script/ObjectSpawner.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToClone;  // 복사할 오브젝트
+    public Transform spawnPoint1;  // 복사된 오브젝트가 생성될 위치
+
+    private GameObject spawnedObject;
+    public GameObject Prefab1;
+    public GameObject Prefab2;
+    public GameObject Prefab3;
+    public GameObject Prefab4;
+    public GameObject Prefab5;
+    public GameObject Prefab6;
+    public GameObject Prefab7;
+    public GameObject Prefab8;
+    public GameObject Prefab9;// B 오브젝트 프리팹
+    public Transform spawnPoint; // B 오브젝트 생성 위치
+    public Animator animator1;
+    public Animator animator2;
+    public Animator animator3;
+    public Animator animator4;
+    public Animator animator5;
+    public Animator animator6;
+    public Animator animator7;
+    public Animator animator8;
+    public Animator animator9;
+    public Canvas parentCanvas;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            // B 오브젝트 생성
+            spawnedObject = Instantiate(Prefab2, spawnPoint.position, spawnPoint.rotation, parentCanvas.transform);
+
+            // C 애니메이션 재생
+            if (animator2 != null)
+            {
+                animator2.SetTrigger("PlayAnimation");
+                Destroy(spawnedObject, 1.0f);
+            }
+            
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            // B 오브젝트 생성
+            spawnedObject = Instantiate(Prefab3, spawnPoint.position, spawnPoint.rotation, parentCanvas.transform);
+
+            // C 애니메이션 재생
+            if (animator3 != null)
+            {
+                animator3.SetTrigger("PlayAnimation");
+                Destroy(spawnedObject, 1.0f);
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            // B 오브젝트 생성
+            spawnedObject = Instantiate(Prefab4, spawnPoint.position, spawnPoint.rotation, parentCanvas.transform);
+
+            // C 애니메이션 재생
+            if (animator4 != null)
+            {
+                animator4.SetTrigger("PlayAnimation");
+                Destroy(spawnedObject, 2.0f);
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            // B 오브젝트 생성
+            spawnedObject = Instantiate(Prefab5, spawnPoint.position, spawnPoint.rotation, parentCanvas.transform);
+
+            // C 애니메이션 재생
+            if (animator5 != null)
+            {
+                animator5.SetTrigger("PlayAnimation");
+                Destroy(spawnedObject, 1.0f);
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            spawnedObject = Instantiate(Prefab6, spawnPoint.position, spawnPoint.rotation, parentCanvas.transform   );
+
+            // C 애니메이션 재생
+            if (animator6 != null)
+            {
+                animator6.SetTrigger("PlayAnimation");
+                Destroy(spawnedObject, 1.0f);
+               
+            }
+        }
+    }
+    void CloneObject()
+    {   
+        Instantiate(objectToClone, spawnPoint1.position, spawnPoint1.rotation);
+        
+            animator6.SetTrigger("PlayAnimation");
+            Destroy(objectToClone, 1.0f);
+
+
+        // 애니메이션 이벤트로 호출될 함수
+    }
+}
