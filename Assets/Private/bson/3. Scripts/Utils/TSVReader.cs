fe2ad1a44e3a9f3329d50c0f2d7736a97ba4cb65
bson343 <byeonggukson@gmail.com>
2024-10-14 05:28:59 +0900

    CSV 대신 TSV로 대신할 경우를 대비한 Parser

---
+++ Assets/Private/bson/3. Scripts/Utils/TSVReader.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class TSVReader
+{
+    static string SPLIT_RE = @"\t(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
+    static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
+    static char[] TRIM_CHARS = { '\"' };
+    static string PARENTHESIS_RE = @"\([^)]*\)";
+
+    public static List<Dictionary<string, object>> Parse(string RawData)
+    {
+        var list = new List<Dictionary<string, object>>();
+
+
+        var lines = Regex.Split(RawData, LINE_SPLIT_RE);
+
+        if (lines.Length <= 1) return list;
+
+        var header = Regex.Split(
+                Regex.Replace(lines[0], PARENTHESIS_RE, string.Empty)
+                , SPLIT_RE
+            );
+        for (var i = 1; i < lines.Length; i++)
+        {
+
+            var values = Regex.Split(lines[i], SPLIT_RE);
+            if (values.Length == 0 || values[0] == "") continue;
+
+            var entry = new Dictionary<string, object>();
+            for (var j = 0; j < header.Length && j < values.Length; j++)
+            {
+                string value = values[j];
+                value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
+                object finalvalue = value;
+                int n;
+                float f;
+                if (int.TryParse(value, out n))
+                {
+                    finalvalue = n;
+                }
+                else if (float.TryParse(value, out f))
+                {
+                    finalvalue = f;
+                }
+                entry[header[j]] = finalvalue;
+            }
+            list.Add(entry);
+        }
+        return list;
+    }
+}
